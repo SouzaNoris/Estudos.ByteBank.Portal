0f08691af2a9e6305b6db76ce5f0f22c2d310db4
Diogo Souza <souzanoris@gmail.com>
2018-01-30 22:57:05 -0200

    Adicionar arquivos de projeto.

---
+++ ByteBank.Portal/Controller/CambioController.cs
@@ -0,0 +1,45 @@
+﻿using ByteBank.Portal.Infraestrutura;
+using ByteBank.Service;
+using ByteBank.Service.Cambio;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal.Controller
+{
+    public class CambioController : ControllerBase
+    {
+        private ICambioService _cambioService;
+
+        public CambioController()
+        {
+            _cambioService = new CambioTesteService();
+        }
+
+        public string MXN()
+        {
+            var valorFinal = _cambioService.Calcular("MXN", "BRL", 1);
+
+            var textoPagina = View();
+
+            var resultado = textoPagina.Replace("VALOR_EM_REAIS", valorFinal.ToString());
+
+            return resultado;
+        }
+
+        public string USD()
+        {
+            var valorFinal = _cambioService.Calcular("USD", "BRL", 1);
+
+            var textoPagina = View();
+
+            var resultado = textoPagina.Replace("VALOR_EM_REAIS", valorFinal.ToString());
+
+            return resultado;
+        }
+    }
+}
---
+++ ByteBank.Portal/Infraestrutura/WebApplication.cs
@@ -0,0 +1,61 @@
+﻿using ByteBank.Portal.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal.Infraestrutura
+{
+    public class WebApplication
+    {
+        private readonly string[] _prefixos;
+
+        public WebApplication(string[] prefixos)
+        {
+            _prefixos = prefixos ?? throw new ArgumentNullException(nameof(prefixos));
+        }
+
+        public void Iniciar()
+        {
+            Console.Write("Aplicação Web Rodando com Reflection");
+
+            while (true)
+                ManipularRequisicao();
+        }
+
+        private void ManipularRequisicao()
+        {
+            var httpListener = new HttpListener();
+
+            foreach (var prefixo in _prefixos)
+            {
+                httpListener.Prefixes.Add(prefixo);
+            }
+
+            httpListener.Start();
+
+            var contexto = httpListener.GetContext();
+            var requisicao = contexto.Request;
+            var resposta = contexto.Response;
+
+
+            var path = requisicao.Url.AbsolutePath;
+
+            if (Utilidades.EhArquivo(path))
+            {
+                var Manipulador = new ManipuladorRequisicaoArquivo();
+                Manipulador.Manipular(resposta, path);
+            }
+            else
+            {
+                var manipulador = new ManipuladorRequisicaoController();
+                manipulador.Manipular(resposta, path);
+            }
+
+            httpListener.Stop();
+        }
+    }
+}
